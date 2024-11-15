a56d7d8e1f7a03f7be1f17d646d9b36a8f482b0c
Fernando Quintanilla <df.quintanillah@gmail.com>
2024-11-14 22:19:09 -0600

    add Chakra UI components and configure TypeScript paths

--- handy-app/src/modules/pages/home/Home.tsx
+++ handy-app/src/modules/pages/home/Home.tsx
@@ -2,7 +2,11 @@ import React from "react";
 // import "./home.css";
 
 const Home: React.FC = () => {
-  return <h1>Welcome to the Home Page</h1>;
+  return (
+    <>
+      <h1>Welcome to the Home Page</h1>
+    </>
+  );
 };
 
 export default Home;

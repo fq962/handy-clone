441276e2cef37c9a2d0872b99ac6c0f7e8089eb3
Fernando Quintanilla <df.quintanillah@gmail.com>
2024-11-14 22:47:55 -0600

    refactor styles and add Navbar component with icons

---
+++ handy-app/src/shared/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number; // Tamaño personalizable
+  color?: string; // Color personalizable
+}
+
+const MenuIcon: React.FC<IconProps> = ({
+  size = 24,
+  color = "currentColor",
+  ...props
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M4 6l16 0" />
+    <path d="M4 12l16 0" />
+    <path d="M4 18l16 0" />
+  </svg>
+);
+
+export default MenuIcon;
